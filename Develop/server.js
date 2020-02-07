f2701f7da993a2402525d3492514ec303ab27944
Seven <sevenspell@gmail.com>
2020-02-07 17:59:19 +1100

    created checks for duplicate id and to ensure the right note is deleted

--- Develop/server.js
+++ Develop/server.js
@@ -4,20 +4,54 @@ var fs = require("fs");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+//use express methods
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+//create array for notes
 var activeNote = [];
 
+//create path to read index.html
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 })
 
+//create path to read notes.html
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
+//create express post method to collect note entry data and log into array, with id
+app.post("/api/notes", function (req, res) {
+
+  var newNote = req.body;
+  id = activeNote.length + 1;
+  newNote.id = id.toString();
+
+  //check id for existing id to prevent duplicates
+  for (var i = 0; i < activeNote.length; i++) {
+
+    if (id != activeNote[i].id) {
+      console.log("ID can be added according to activeNote.length");
+    } else {
+      console.log("ID already exists. Use the next available ID.");
+      id++;
+      newNote.id = id.toString();
+    } 
+  }
+  //log data into array
+  activeNote.push(newNote);
+  res.json(newNote);
+
+  //write array data into db.json
+  fs.writeFile("./db/db.json", JSON.stringify(activeNote), function (err) {
+    if (err) throw err;
+    console.log("Data has been saved to database");
+  })
+});
+
+//create express get method to send db data in JSON format to display on HTML
 app.get("/api/notes", function (req, res) {
   fs.readFile("./db/db.json", "utf-8", function (err, data) {
     if (err) throw err;
@@ -25,26 +59,12 @@ app.get("/api/notes", function (req, res) {
   })
 })
 
-app.post("/api/notes", function (req, res) {
-
-  var newNote = req.body;
-  id = activeNote.length + 1;
-  newNote.id = id.toString();
-  activeNote.push(newNote);
-  res.json(newNote);
-
-  fs.writeFile("./db/db.json", JSON.stringify(activeNote), function (err) {
-    if (err) throw err;
-    console.log("Data has been saved to database");
-  })
-});
-
-app.get("/api/notes/:character", function(req, res) {
+//create express get method to match chosen note item with item in array based on ID
+app.get("/api/notes/:character", function (req, res) {
   var chosen = req.params.character;
-  console.log(chosen + " chosen");
-  console.log(activeNote[i].id + " id");
 
   for (var i = 0; i < activeNote.length; i++) {
+
     if (chosen === activeNote[i].id) {
       return res.json(activeNote[i]);
     }
@@ -52,37 +72,31 @@ app.get("/api/notes/:character", function(req, res) {
   return res.json(false);
 });
 
-app.delete("/api/notes/:character", function(req, res){
+//create express method to find item chosen by user based on ID and splice away from existing array
+app.delete("/api/notes/:character", function (req, res) {
   var chosen = req.params.character;
   console.log(chosen + " chosen to delete")
 
   for (var i = 0; i < activeNote.length; i++) {
-    
     if (chosen === activeNote[i].id) {
-      console.log(activeNote[i].id + " id in array")
-      activeNote.splice(chosen-1, 1);
-      console.log(JSON.stringify(activeNote) + " new activeNote")
+      activeNote.splice(i, 1);
 
       fs.writeFile("./db/db.json", JSON.stringify(activeNote), function (err) {
         if (err) throw err;
         console.log("Data has been saved to database");
-    
       })
       return res.json(activeNote);
     }
   }
-
-
   return res.json(false);
-
-
 })
 
-
+//create express method to display error message if pathway is incorrect
 app.use(function (req, res, next) {
   res.status(404).send("<h2>The page you're looking for is unavailable. Please check if the address is correct and try again. Good luck!</h2>");
 })
 
+//create express method to listen to server
 app.listen(PORT, function () {
   console.log("App listening on http://localhost:" + PORT);
 });
